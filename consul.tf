0d55fbd50304a5a553913d20d4fcb0f8e1f1d268
Bren Briggs <briggs.brenton@gmail.com>
2017-04-13 15:52:08 -0400

    Configure security group for consul

--- consul.tf
+++ consul.tf
@@ -48,14 +48,76 @@ variable "user" {
   default = "root"
 }
 
-variable "vpc_security_group_ids" {
-  type        = "list"
-  description = "List of security group IDs to associate instances with (VPC only)"
+variable "vpc" {
+  type        = "string"
+  description = "VPC in which to deploy Consul"
 }
 
 #######################################
 provider "aws" {
   region = "${var.region}"
+}
+
+resource "aws_security_group" "consul" {
+  name_prefix = "${var.prefix}"
+  description = "Allow consul traffic"
+  vpc_id = "${var.vpc}"
+}
+
+resource "aws_security_group_rule" "ssh" {
+  type            = "ingress"
+  from_port       = 22
+  to_port         = 22
+  protocol        = "tcp"
+  security_group_id = "${aws_security_group.consul.id}"
+}
+
+resource "aws_security_group_rule" "consul_rpc" {
+  type = "ingress"
+  from_port = "8300"
+  to_port = "8300"
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.consul.id}"
+}
+
+resource "aws_security_group_rule" "consul_serf_lan" {
+  type = "ingress"
+  from_port = "8301"
+  to_port = "8301"
+  protocol = "all"
+  security_group_id = "${aws_security_group.consul.id}"
+}
+
+resource "aws_security_group_rule" "consul_serf_wan" {
+  type = "ingress"
+  from_port = "8302"
+  to_port = "8302"
+  protocol = "all"
+  security_group_id = "${aws_security_group.consul.id}"
+}
+
+resource "aws_security_group_rule" "consul_cli_rpc" {
+  type = "ingress"
+  from_port = "8400"
+  to_port = "8400"
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.consul.id}"
+}
+
+resource "aws_security_group_rule" "consul_http_api" {
+  type = "ingress"
+  from_port = "8500"
+  to_port = "8500"
+  protocol = "tcp"
+  security_group_id = "${aws_security_group.consul.id}"
+}
+
+resource "aws_security_group_rule" "consul_dns_interface" {
+  type = "ingress"
+  from_port = "8600"
+  to_port = "8600"
+  protocol = "all"
+  security_group_id = "${aws_security_group.consul.id}"
 }
 
 resource "aws_instance" "consul" {
@@ -67,7 +129,7 @@ resource "aws_instance" "consul" {
   key_name               = "${var.key_name}"
   region                 = "${var.region}"
   subnet_id              = "${var.subnet_id}"
-  vpc_security_group_ids = "${var.vpc_security_group_ids}"
+  vpc_security_group_ids = ["${aws_security_group.consul.id}"]
 
   connection {
     type        = "ssh"
